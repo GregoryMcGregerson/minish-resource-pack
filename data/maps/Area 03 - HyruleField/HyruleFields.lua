9f0c718e5b852804f685d561c8382877ed26ab0b
Crudiant <59270799+Crudiant@users.noreply.github.com>
2021-07-28 01:09:02 +0200

    Current progress on Hyrule Town and Fields (#1)
    * Add files via upload
    
    * Update project_db.dat

--- data/maps/Area 03 - HyruleField/HyruleFields.lua
+++ data/maps/Area 03 - HyruleField/HyruleFields.lua
@@ -1,4 +1,4 @@
--- Lua script of map 02_00.
+-- Lua script of map Area 03 - HyruleField/HyruleFields.
 -- This script is executed every time the hero enters this map.
 
 -- Feel free to modify the code below.
